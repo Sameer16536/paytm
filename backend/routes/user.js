05ebddc2b2f1697386a06a496f32361b0c698a25
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-18 23:30:04 +0530

    signup route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,12 +5,18 @@ const {User}  = require('../db')
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../config')
 
-
+//During SignIn
 const userInputSchema = zod.object({
     username: zod.string().min(3).max(30).email(),
     password: zod.string().min(6),
     firstName: zod.string().max(50),
     lastName: zod.string().max(50),
+})
+
+//During SignUp 
+const signUpSchema = zod.object({
+    username: zod.string().min(3).max(30).email(),
+    password: zod.string().min(6)
 })
 
 router.post('/signup', async (req, res) => {
@@ -43,4 +49,28 @@ router.post('/signup', async (req, res) => {
     res.json({msg:"Tuser created successfully",token:token})
 
 })
+
+//Signin
+router.post('/signin', async (req, res) => {
+    const result = userInputSchema.safeParse(req.body)    
+    if(!result.success){
+        return res.status(400).json({ error: 'Invalid input' })
+        }
+    //Find the user:
+    const user = await User.findOne({
+        username:req.body.username,
+        password:req.body.password
+    })
+    
+
+    //Validate the token
+    if(user){{
+        const token = jwt.sign({userId:user._id},JWT_SECRET)
+    }}
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+
+})
+
 module.exports = router;
